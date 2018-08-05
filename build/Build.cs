89c2a1cc4d6a76be745a0b2a91fc3f18cbb71e3e
Georg Dangl <georg@dangl.me>
2018-08-05 13:48:16 +0200

    Specifiy framework for xUnit tests

--- build/Build.cs
+++ build/Build.cs
@@ -116,7 +116,7 @@ class Build : NukeBuild
         {
             void TestXunit()
                 => Xunit2(GlobFiles(SolutionDirectory, $"*/bin/{Configuration}/net46*/Nuke.*.Tests.dll").NotEmpty(),
-                    s => s.AddResultReport(Xunit2ResultFormat.Xml, OutputDirectory / "tests.xml"));
+                    s => s.AddResultReport(Xunit2ResultFormat.Xml, OutputDirectory / "tests.xml").SetFramework("net461"));
 
             TestXunit();
         });
